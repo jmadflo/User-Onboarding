4bec44b200dfb82db559fd223bbd490afe3e965f
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-03-25 18:04:18 -0600

    MVP + stretch

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,129 @@
+import React, {useState, useEffect}from 'react'
+import * as yup from 'yup'
+import axios from 'axios'
+
+const Form = () => {
+
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+
+    const [post, setPost] = useState([])
+
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        role: "",
+        password: "",
+        terms: ""
+    });
+
+    const [formErrors, setFormErrors] = useState({
+        name: "",
+        email: "",
+        role: "",
+        password: "",
+        terms: ""
+    });
+
+    useEffect(() => {
+        formSchema.isValid(formState)
+            .then(valid => {
+                setSubmitDisabled(!valid);
+            });
+    }, [formState]);
+
+    const formSchema = yup.object().shape({
+        name: yup.string()
+            .required("Please input your name."),
+        email: yup.string()
+            .email("This needs to be a valid email address.")
+            .required("Please input a valid email address."),
+        role: yup.string(),
+        password: yup.string()
+            .required("Please enter a password"),
+        terms: yup.boolean()
+            .oneOf([true], "Please agree to terms of service, preferably without reading them.")
+    })
+
+    const validateData = event => {
+        yup
+            .reach(formSchema, event.target.name)
+            .validate(event.target.value)
+            .then(valid => {
+                console.log(valid)
+                setFormErrors({
+                  ...formErrors,
+                  [event.target.name]: ""
+                });
+            })
+            .catch(error => {
+                console.log(error)
+                setFormErrors({
+                  ...formErrors,
+                  [event.target.name]: error.errors[0]
+                });
+            });
+    }
+
+    const updateForm = event => {
+        event.persist();
+        validateData(event);
+        setFormState({
+            ...formState, 
+            [event.target.name]: event.target.type === "checkbox" ? event.target.checked: event.target.value});
+      };
+
+    const submitForm = event => {
+        event.preventDefault();
+        axios
+          .post("https://reqres.in/api/users", formState)
+          .then(response => {
+            console.log("Posted successfully: ", post);
+            setPost(response.data);
+            setFormState({
+                name: "",
+                email: "",
+                role: "",
+                password: "",
+                terms: ""
+            });
+          })
+          .catch(error => console.log("Post was not successful: ", error.response));
+      };
+
+    return (
+        <form onSubmit={submitForm}>
+            <label htmlFor="name">
+                Name
+                <input id="name" name="name" type="text" value={formState.name}  onChange={updateForm}/>
+                {formErrors.name > 0 ? <p className="error">{formErrors.name}</p> : null}
+            </label>
+            <label htmlFor="email">
+                Email 
+                <input id="email" name="email" type="text" value={formState.email} onChange={updateForm}/>
+                {formErrors.email ? <p className="error">{formErrors.email}</p> : null}
+            </label>
+            <label htmlFor="role">
+                Role
+                <select id="role" name="role" onChange={updateForm}>
+                    <option value="Frontend Developer">Frontend Developer</option>
+                    <option value="Backend Developer">Backend Developer</option>
+                    <option value="Full Stack Developer">Full Stack Developer</option>
+                </select>
+                {formErrors.role ? <p className="error">{formErrors.role}</p> : null}
+            </label>
+            <label htmlFor="password">
+                Password
+                <input id="password" name="password" type="password" value={formState.password} onChange={updateForm}/>
+                {formErrors.password ? <p className="error">{formErrors.password}</p> : null}
+            </label>
+            <label htmlFor="terms" className="terms">
+                <input type="checkbox" name="terms" checked={formState.terms} onChange={updateForm}/>
+                Terms & Conditions
+            </label>
+            <pre>{JSON.stringify(post, null, 2)}</pre>
+            <input type="submit" disabled={submitDisabled} />
+        </form>
+    )
+}
+
+export default Form
